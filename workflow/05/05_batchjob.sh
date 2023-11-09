32a0584b706eddc694fe31866dfab9343330c460
antonigoo <antoni.golos@csc.fi>
2023-11-09 17:10:10 +0200

    step 05: adjust batchjob

--- workflow/05/05_batchjob.sh
+++ workflow/05/05_batchjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=05_normalize_to_ground
 #SBATCH --account=project_2008498
-#SBATCH --time=00:15:00
+#SBATCH --time=00:5:00
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=15G
 #SBATCH --partition=small
