7a35e2ec656aed6d30c2462483e42ab8af1da386
Antoni Golos <antongoo@r07c05.bullx>
2024-04-19 17:58:08 +0300

    snakemake perf test setup

--- snakemake/batchjob.sh
+++ snakemake/batchjob.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=snakemake_test
+#SBATCH --job-name=snakemake_batchfile
 #SBATCH --account=project_2008498
 #SBATCH --time=00:20:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=40GB
 #SBATCH --partition=small
 #SBATCH --output=slurm-%j.out
@@ -12,5 +11,5 @@
 module -q purge
 export PATH="/projappl/project_2003180/samantha/bin:$PATH" 
 
-snakemake --cores 4 --keep-going --config BASE_PATH="/scratch/project_2008498/antongoo/fgi/snakemake_many_files" --wms-monitor http://127.0.0.1:5000 
+snakemake --cores 1 --keep-going --config BASE_PATH="/scratch/project_2008498/antongoo/fgi/snakemake"
 # snakemake -s Snakefile_SLURM --keep-going  --jobs 4 --executor slurm --default-resources slurm_account=project_2008498 slurm_partition=small  --config BASE_PATH="/scratch/project_2008498/antongoo/fgi/snakemake_many_files"
