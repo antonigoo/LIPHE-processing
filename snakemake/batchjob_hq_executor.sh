8caea986810f95711990ad3dcd1adac0e25ada7a
Antoni Golos <antongoo@r07c52.bullx>
2024-05-28 11:50:56 +0300

    update resources

--- snakemake/batchjob_hq_executor.sh
+++ snakemake/batchjob_hq_executor.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=snakemake_hq
 #SBATCH --account=project_2008498
 #SBATCH --time=00:20:00
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=40GB
 #SBATCH --partition=small
 #SBATCH --output=slurm-%j.out
